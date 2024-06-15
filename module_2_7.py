ac82bec04c3889abc9c14216e929160afcb4906f
DmitriiMir <dmitrii.miroshnik.92@gmail.com>
2024-06-15 22:19:21 +0300

    Module_2

---
+++ module_2_7.py
@@ -0,0 +1,29 @@
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+
+# Вызов функции с разным количеством аргументов
+print_params()
+print_params(10)
+print_params(10, 'новая строка')
+print_params(b=25)
+print_params(c=[1, 2, 3])
+
+# Создаем список с тремя элементами разных типов
+values_list = [21, 'Дмитрий', False]
+
+# Создаем словарь
+values_dict = {
+    "a": 100,
+    "b": "Мир",
+    "c": [1, 2, 3]
+}
+
+# Распаковка параметров
+print_params(*values_list)
+print_params(**values_dict)
+
+# Создаем список с двумя элементами разных типов
+values_list_2 = [5, 'для_примера']
+
+# Проверяем работу функции
+print_params(*values_list_2, 42)
