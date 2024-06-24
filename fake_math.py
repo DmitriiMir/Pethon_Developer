24e2e689bbae1171b08b268009ebcf45b0dfc25f
DmitriiMir <dmitrii.miroshnik.92@gmail.com>
2024-06-24 13:56:04 +0300

    module_4

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+'''Модуль для учебного проекта "Модули и проекты". Функция fake_divide.'''
+
+def fake_divide(first, second):
+    if second == 0:
+        return 'Ошибка'
+    return first / second
