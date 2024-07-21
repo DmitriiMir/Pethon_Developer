dab3c40e5ebd3c2f3ef5639fa2bdd8dca878f2a4
DmitriiMir <dmitrii.miroshnik.92@gmail.com>
2024-07-21 16:36:53 +0300

    module_6_1

--- module_6_1.py
+++ module_6_1.py
@@ -1,13 +1,17 @@
 class Animal:
     alive = True
     fed = False
+
     def __init__(self, name):
         self.name = name
 
+
 class Plant:
     edible = False
+
     def __init__(self, name):
         self.name = name
+
 
 class Mammal(Animal):
     def eat(self, food):
@@ -18,6 +22,7 @@ class Mammal(Animal):
             print(f"{self.name} не стал есть {food.name}")
             self.alive = False
 
+
 class Predator(Animal):
     def eat(self, food):
         if food.edible:
@@ -27,37 +32,34 @@ class Predator(Animal):
             print(f"{self.name} не стал есть {food.name}")
             self.alive = False
 
+
 class Flower(Plant):
     def __init__(self, name):
         self.name = name
 
+
 class Fruit(Plant):
     edible = True
+
     def __init__(self, name):
         self.name = name
 
 
+# Выполняемый код(для проверки):
 
-
-
-# Создаем объекты классов
 a1 = Predator('Волк с Уолл-Стрит')
 a2 = Mammal('Хатико')
 p1 = Flower('Цветик семицветик')
 p2 = Fruit('Заводной апельсин')
 
-# Выводим имена объектов
 print(a1.name)
 print(p1.name)
 
-# Проверяем начальное состояние объектов
 print(a1.alive)
 print(a2.fed)
-
-# Кормим животных
 a1.eat(p1)
 a2.eat(p2)
-
-# Проверяем состояние объектов после кормления
 print(a1.alive)
 print(a2.fed)
+
+# Что произошло: Хищник попытался съесть цветок и погиб, млекопитающее съело фрукт и насытилось.
